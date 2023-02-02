88d7deb4cf0cef095b08089784e811440488fd3c
Martin Devillers <martin@devillers.nl>
2023-02-02 18:18:44 -0500

    Expose environment variable

--- client/lib/graphql.ts
+++ client/lib/graphql.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 
-const httpLink = new HttpLink({ uri: process.env.API || "http://localhost:4000" })
+const httpLink = new HttpLink({ uri: process.env.NEXT_PUBLIC_API || "http://localhost:4000" })
 
 export const client = new ApolloClient({
   link: httpLink,
